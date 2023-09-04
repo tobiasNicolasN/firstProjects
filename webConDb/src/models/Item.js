125f5e22b59c97d8f5f79075a2ccfd3cd7cb14ab
Tobias Nuñez <135732822+tobiasNicolasN@users.noreply.github.com>
2023-09-04 07:50:11 -0300

    Delete webConDb directory

--- webConDb/src/models/Item.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var ItemSchema = new mongoose_1.default.Schema({
-    name: { type: String, required: true },
-    description: String,
-});
-exports.default = mongoose_1.default.model('Item', ItemSchema);
